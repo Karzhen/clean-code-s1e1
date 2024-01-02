c2e899a48e63ae28a5938287e7e4fbfcaeee6e6d
Egor Cherkashin <egor9298@mail.ru>
2024-01-03 01:57:22 +0300

    feat: add the same classes into JS file LANG=en_EN Wed, Jan 03, 2024 01:57:22 AM

--- app.js
+++ app.js
@@ -28,18 +28,21 @@ var createNewTaskElement = function(taskString){
     var deleteButtonImg= document.createElement("img"); //delete button image
 
     label.innerText = taskString;
-    label.className = 'task';
+    label.className = 'label task';
 
     //Each elements, needs appending
     checkBox.type = "checkbox";
+    checkBox.className = "checkbox";
+
     editInput.type = "text";
-    editInput.className = "task";
+    editInput.className = "task input-task";
 
     editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className = "edit";
+    editButton.className = "button edit";
 
-    deleteButton.className = "delete";
+    deleteButton.className = "button delete";
     deleteButtonImg.src = './remove.svg';
+    deleteButtonImg.className = "button-image";
     deleteButton.appendChild(deleteButtonImg);
 
 
@@ -49,10 +52,9 @@ var createNewTaskElement = function(taskString){
     listItem.appendChild(editInput);
     listItem.appendChild(editButton);
     listItem.appendChild(deleteButton);
+    listItem.className = "section-item";
     return listItem;
 }
-
-
 
 var addTask = function(){
     console.log("Add Task...");
